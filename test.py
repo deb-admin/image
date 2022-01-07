16c835c1d2ef5b1a1191b838d99612cdef42d131
XiaoD <62047496+deb-admin@users.noreply.github.com>
2022-01-07 19:53:33 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,50 +1,47 @@
-import codecs
-# input_file = codecs.open('05.md', mode="r", encoding="utf-8")
-# text = input_file.read()
-# html = markdown.markdown(text, extensions=extensions)
-# print(html)
-# output_file = codecs.open('1.html', "w", encoding="utf-8", errors="xmlcharrefreplace")
-# output_file.write(html)
-
-
-
 import locale
 from dialog import Dialog
+import os
 
+Version = 1.0
 # This is almost always a good thing to do at the beginning of your programs.
 locale.setlocale(locale.LC_ALL, '')
 
 # You may want to use 'autowidgetsize=True' here (requires pythondialog >= 3.1)
 d = Dialog(dialog="dialog")
-# Dialog.set_background_title() requires pythondialog 2.13 or later
-d.set_background_title("My little program")
-# For older versions, you can use:
-#   d.add_persistent_args(["--backtitle", "My little program"])
 
-# In pythondialog 3.x, you can compare the return code to d.OK, Dialog.OK or
-# "ok" (same object). In pythondialog 2.x, you have to use d.DIALOG_OK, which
-# is deprecated since version 3.0.0.
-if d.yesno("Are you REALLY sure you want to see this?") == d.OK:
-    d.msgbox("You have been warned...")
 
-    # We could put non-empty items here (not only the tag for each entry)
-    code, tags = d.checklist("What sandwich toppings do you like?",
-                             choices=[("Catsup", "",             False),
-                                      ("Mustard", "",            False),
-                                      ("Pesto", "",              False),
-                                      ("Mayonnaise", "",         True),
-                                      ("Horse radish","",        True),
-                                      ("Sun-dried tomatoes", "", True)],
-                             title="Do you prefer ham or spam?",
-                             backtitle="And now, for something "
-                             "completely different...")
+def get_args():
+    parser = argparse.ArgumentParser(description="Python TUI Upload Project")
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s {}'.format(str(Version)))
+    parser.parse_args()
+    return True
+
+def find_file(path):
+    files = []
+    for dirpath, dirname, filenames in os.walk(path):
+        for filename in filenames:
+            files.append(os.path.join(dirpath, filename))
+    return files
+
+if __name__ == '__main__':
+    get_args()
+    files_list = []
+    files = find_file("/mnt")
+    for file in files:
+        files_list.append((file, "", True))
+
+    code, tags = d.checklist("Please select the file(s) you want to upload:", choices=files_list, title="Select files")
     if code == d.OK:
-        # 'tags' now contains a list of the toppings chosen by the user
-        pass
-else:
-    code, tag = d.menu("OK, then you have two options:",
-                       choices=[("(1)", "Leave this fascinating example"),
-                                ("(2)", "Leave this fascinating example")])
-    if code == d.OK:
-        # 'tag' is now either "(1)" or "(2)"
-        pass
+        for tag in tags:
+            file_size = os.path.getsize(tag)
+            target_size = 0
+            record_size = file_size / 100
+            text = "Uploading {} ....".format(tag)
+            d.gauge_start(text, percent=0, title="Uploading")
+            for i in range(1, 101):
+                d.gauge_update(i, update_text=False)
+            exit_code = d.gauge_stop()
+    else:
+        print("You cancelled the dialog!")
+
+    print(files)
